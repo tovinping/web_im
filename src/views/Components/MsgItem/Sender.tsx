2fe508929b3b58d7bf6472ab0c8933be70349da5
tovinping <tovinping@qq.com>
2022-01-29 11:36:23 +0800

    消息列表布局调整添加图像显示

--- src/views/Components/MsgItem/Sender.tsx
+++ src/views/Components/MsgItem/Sender.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React from 'react'
+import { BaseAvatar } from 'src/components/Avatar'
+import { useRootState } from 'src/store'
 
-export function Sender() {
-  return <div></div>
+export default function Sender({ senderId }: { senderId: string }) {
+  const avatarUrl = useRootState(state => state.user[senderId]?.avatar)
+  return <BaseAvatar url={avatarUrl} />
 }
--- src/views/Components/MsgItem/index.tsx
+++ src/views/Components/MsgItem/index.tsx
@@ -3,15 +3,21 @@ import classnames from 'classnames'
 import { IMsg } from 'src/interface'
 import { useRootState } from 'src/store'
 import MsgText from '../MsgItemText'
+import Sender from './Sender'
 import style from './index.module.scss'
 
 export default function MsgItem(props: IMsg) {
   const myId = useRootState(state => state.global.account)
   const isMe = props.senderId === myId
   return (
-    <div className={classnames(style.container, isMe && style.isMe)}>
-      <div className={classnames(style.content, isMe && style.isMe)}>
-        <MsgText {...props} />
+    <div className={classnames(style.msgItem, isMe && style.isMe)}>
+      <div className={style.container}>
+        <div className={classnames(style.sender, isMe && style.isMe)}>
+          <Sender senderId={props.senderId} />
+        </div>
+        <div className={classnames(style.content, isMe && style.isMe)}>
+          <MsgText {...props} />
+        </div>
       </div>
     </div>
   )
